82097c65958e68ca21a78a756c5507492eb75217
xixi-xix <akunxav300@gmail.com>
2026-04-24 21:35:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,7 @@ def predict():
 
     return jsonify({'penyakit': hasil[0]})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+if __name__=='__main__':
+    port = int(os.environ.get('PORT,5000))
+app.run(host='0.0.0.0', port=port)
